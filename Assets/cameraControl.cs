37634017a287d4a86fed690127353615b62ed52c
Rachel Moeller <parrishwork@gmail.com>
2017-10-24 15:32:35 -0400

    Added a Script

---
+++ Assets/cameraControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//woo yeah
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
